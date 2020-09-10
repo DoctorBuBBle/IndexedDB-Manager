3a2a3a9a07f60d10e51acecadf2bd97f5ea79c83
Sebastian Paas <s-paas@t-online.de>
2020-09-10 10:25:57 -0700

    first tries with the overview of tabels

---
+++ src/components/Spinner/LoadingSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoadingSpinner.css';
+
+export default function(){
+    return (
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    );
+}
